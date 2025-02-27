1883b2c8c766b777d8f4f522a8bf65ffc94f862a
jameslazo <james.lazo+git@protonmail.com>
2025-02-26 22:05:43 -0700

    feat: k8s + fzf
    Add describe pods and exec -it with fzf integration

--- bash/.bintegrations.sh
+++ bash/.bintegrations.sh
@@ -10,6 +10,8 @@ alias drmi='docker rmi $(docker images --format "{{.ID}} {{.Repository}} {{.Tag}
 alias ds='docker stop $(docker ps -a --format "{{.ID}} {{.Image}} {{.Names}} {{.RunningFor}}" | fzf --reverse | awk  '"'"'{print $1}'"'"')'
 alias dsci='c=$docker ps | fzf --reverse; [ -n "$c" ] && docker stop "$c" && docker rm "$c"; i=$(docker images | fzf --reverse); [ -n "$i" ] && docker rmi $"i"'
 alias fz="fzf --reverse --preview 'less {}'"
+alias k8dp='microk8s kubectl describe pod $(microk8s kubectl get pods -A | fzf --reverse | awk  '"'"'{print $2}'"'"')'
+alias k8ex='microk8s kubectl exec -it $(microk8s kubectl get pods -A | fzf --reverse | awk  '"'"'{print $2}'"'"')' -- bash
 #-----#
 
 
