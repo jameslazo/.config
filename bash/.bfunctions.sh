c0075b3517df2b7c82acb49167f360a56127bc35
jameslazo <james.lazo+git@protonmail.com>
2025-03-15 17:18:16 -0700

    feat: tmux attach/new session feat: update k9s feat: update fzf

--- bash/.bfunctions.sh
+++ bash/.bfunctions.sh
@@ -1,3 +1,15 @@
 gp() {
   microk8s kubectl get pods -n $1
 }
+
+ta() {
+  tmux attach -t $HOSTNAME &> /dev/null || tmux new -s $HOSTNAME
+}
+
+upk9s() {
+  wget https://github.com/derailed/k9s/releases/latest/download/k9s_linux_amd64.deb && sudo apt install ./k9s_linux_amd64.deb && rm k9s_linux_amd64.deb
+}
+
+upfzf() {
+  git clone --depth 1 https://github.com/junegunn/fzf.git /tmp/.fzf && cp -r /tmp/.fzf ~ && ~/.fzf/install
+}
