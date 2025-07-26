e41e5e423baec7a29d035d630218ed7a621261f6
James Lazo (jazo) <james.lazo+git@protonmail.com>
2025-07-26 08:03:20 -0700

    feat: open last cursor position

--- nvim/init.lua
+++ nvim/init.lua
@@ -153,3 +153,13 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.shiftwidth = 2
   end,
 })
+
+vim.api.nvim_create_autocmd({"BufReadPost"}, {
+    pattern = {"*"},
+    callback = function()
+        if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+            vim.api.nvim_exec2("normal! g'\"", {output = false})
+        end
+    end
+})
+
